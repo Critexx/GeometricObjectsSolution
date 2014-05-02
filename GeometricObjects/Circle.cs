2118666c9d5334011a4f4f17132aad1b8e99b4f1
Pascal <pascal.schaffner@hotmail.com>
2014-05-02 02:22:30 +0200

    first commit

---
+++ GeometricObjects/Circle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricObjects
+{
+    public class Circle
+    {
+        // -------- Eigenschaftsmethoden ----------
+
+        public double YCoordinate {get; set;}
+        public double XCoordinate {get; set;}
+
+        private int _Radius;
+        public int Radius
+        {
+            get
+            {
+                return _Radius;
+            }
+            set
+            {
+                if (value >= 0)
+                    _Radius = value;               
+                else
+                    Console.WriteLine("Der eingegeben Wert darf nicht negativ sein.");
+            }
+        }
+
+        // -------- Methoden ----------
+
+        // Fläche
+        public double GetArea()
+        {
+            double area = Math.Pow(Radius, 2);
+            return area;
+        }
+        // Umfang
+        public double GetCircumference()
+        {
+            double circumference = 2 * Radius * Math.PI;
+            return circumference;
+        }
+
+        public void Move(double dx, double dy)
+        {
+            XCoordinate += dx;
+            YCoordinate += dy;
+        }
+    }
+}
---
+++ GeometricObjects/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle kreis = new Circle();
+            kreis.Radius = 12;
+            kreis.XCoordinate = 100;
+            kreis.YCoordinate = 100;
+
+            kreis.Move(20, 60);
+
+            // Ausgabe:
+            Console.WriteLine("Der Kreisradius beträgt {0}", kreis.Radius);
+            Console.WriteLine("Die Fläche beträgt {0}", kreis.GetArea());
+            Console.WriteLine("Der Umfang beträgt {0}", kreis.GetCircumference());
+            Console.WriteLine("Der Kreis befindet sich auf X: {0} Y: {1}", kreis.XCoordinate, kreis.YCoordinate);
+            Console.ReadKey();
+        }
+    }
+}
